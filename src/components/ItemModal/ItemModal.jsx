1d9ee61ad7f93d7a2eecebb63a10774fd9ec38d3
Robert_Rodriguez <rob915@gmail.com>
2024-08-14 07:20:21 -0700

    bug fixes: correcting fitlering of cards in Main.jsx and alt text in ItemModal.jsx

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -9,7 +9,7 @@ function ItemModal({ isOpen, onClose, card }) {
           type="button"
           className="modal__close modal__close_image"
         ></button>
-        <img src={card.link} alt="" className="modal__image" />
+        <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer"></div>
         <h2 className="modal__caption">{card.name}</h2>
         <p className="modal__weather">Weather: {card.weather}</p>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/se_project_react/", // add this field
   plugins: [react()],
   server: {
     port: 3000,

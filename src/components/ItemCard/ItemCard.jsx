44e5e6c126a45459bece73787c0471205586e87a
ROBERT RODRIGUEZ <rob915@gmail.com>
2024-12-19 11:32:57 -0800

    feature: added conditional rendering to like buttons based on isLoggedIn and item.owner

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -4,33 +4,28 @@ import { likeItem, dislikeItem } from "../../utils/api";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function ItemCard({ item, onCardClick, clothingItems, onCardLike }) {
-  const { currentUser } = useContext(CurrentUserContext);
+  const { currentUser, isLoggedIn } = useContext(CurrentUserContext);
 
   const handleCardClick = () => {
     onCardClick(item);
   };
 
-  const onLikeItem = () => {
-    likeItem(_id, localStorage.getItem("jwt"));
-  };
-
-  // useEffect(() => {
-  //   if()
-  // });
   const isLiked = item.likes.some((like) => like === currentUser._id);
 
   return (
     <li className="card">
       <div className="card__header">
         <h2 className="card__title">{item.name}</h2>
-        <button
-          onClick={() => {
-            onCardLike(isLiked, item);
-          }}
-          className={`card__like-button ${
-            isLiked && "card__like-button_active"
-          }`}
-        ></button>
+        {isLoggedIn && currentUser._id === item.owner && (
+          <button
+            onClick={() => {
+              onCardLike(isLiked, item);
+            }}
+            className={`card__like-button ${
+              isLiked && "card__like-button_active"
+            }`}
+          ></button>
+        )}
       </div>
       <img
         onClick={handleCardClick}
